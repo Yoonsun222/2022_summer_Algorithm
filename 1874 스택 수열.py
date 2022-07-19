5958720f1e5f1191d66129d10a3c3b6f1665bb1f
Yoonsun222 <profit_sun@naver.com>
2022-07-19 09:32:52 +0900

    2022-07-18

--- 1874 스택 수열.py
+++ 1874 스택 수열.py
@@ -1,3 +1,7 @@
+import sys 
+input = sys.stdin.readline
+
+
 N = int(input())
 
 lst = [int(input()) for i in range(N)]
@@ -5,18 +9,18 @@ lst = [int(input()) for i in range(N)]
 ans_pm, tmp = [], []
 cnum, idx = 1, 0
 
-while idx < N:
-    if tmp and tmp[-1] == lst[idx]:
-        idx += 1
-        tmp.pop()
-        ans_pm.append('-')
-    elif cnum > N:
+while idx < N:  #idx로 lst의 0부터 N-1까지 검토
+    if tmp and tmp[-1] == lst[idx]: #만약 tmp에 값이 존재 하고 tmp의 맨 마지막 값이 해당 인덱스의 lst값과 같으면
+        idx += 1    #idx를 +1을 하여 이후에 lst[idx+1]값을 찾게 만들고
+        tmp.pop()   #tmp[-1] 값을 pop한다.
+        ans_pm.append('-') #그리고 정답에 -를 집어 넣음
+    elif cnum > N: # 만약 cnum이 idx보다 커지면 'NO'출력
         print('NO')
         break
     else:
-        tmp.append(cnum)
-        ans_pm.append('+')
-        cnum += 1
+        tmp.append(cnum) #위의 모든 경우에 해당이 안되면 cnum을 tmp에 append
+        ans_pm.append('+') # 정답에 + 넣음
+        cnum += 1 # cnum +1
 
 if idx == N:
     for i in ans_pm:
