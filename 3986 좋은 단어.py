58364babb894e947a065eaa24b216a3bf80a9775
Yoonsun222 <profit_sun@naver.com>
2022-07-20 11:41:53 +0900

    2022-07-20

---
+++ 3986 좋은 단어.py
@@ -0,0 +1,29 @@
+
+import sys 
+input = sys.stdin.readline
+
+N = int(input())
+
+answer= 0
+for _ in range(N):
+    word = list(input())[:-1]
+    stack = []
+    #print(word)
+    if len(word) % 2:
+        continue
+    while word:
+        cn = word.pop()
+        #print(word ,stack, cn)
+        if stack and stack[-1] == cn:
+            stack.pop()
+        else:
+            stack.append(cn)
+
+    if stack:
+        continue
+    else:
+        answer += 1
+        
+        
+
+print(answer)
