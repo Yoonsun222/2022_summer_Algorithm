c4a67e9ede53e2ff0980a5db1e1d721a86c781bb
Yoonsun222 <profit_sun@naver.com>
2022-09-09 21:08:59 +0900

    2022-09-09

--- 7주차 DP/1149 - RGB거리.py
+++ 7주차 DP/1149 - RGB거리.py
@@ -1,11 +1,26 @@
 #oxo 가능 ooo 불가능 oxox 가능
 
 import sys
+input = sys.stdin.readline
 
 
 N = int(input())
 
-board = []
-for _ in range(N):
-    board.append(list(map(int,input().split())))
+red, green, blue = [],[],[]
+dp = [[0 for _ in range(3)] for _ in range(N)]
 
+for _ in range(N):
+    R,G,B = map(int,input().split()) 
+    red.append(R)
+    green.append(G)
+    blue.append(B)
+
+
+dp[0][0], dp[0][1], dp[0][2] = red[0], green[0], blue[0]
+
+for i in range(1,N):
+    dp[i][0] = red[i] + min(dp[i-1][1], dp[i-1][2])
+    dp[i][1] = green[i] + min(dp[i-1][0], dp[i-1][2])
+    dp[i][2] = blue[i] + min(dp[i-1][1], dp[i-1][0])
+
+print(min(dp[-1]))
--- 7주차 DP/1904 - 01타일 .py
+++ 7주차 DP/1904 - 01타일 .py
@@ -5,4 +5,4 @@ dp = [1,2] + [0 for _ in range(N-2)]
 for i in range(2,N):
     dp[i] = (dp[i-1] +dp[i-2]) % 15746
 
-print(dp[-1])
+print(dp[N-1])
